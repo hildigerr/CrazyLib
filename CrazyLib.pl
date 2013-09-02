d76a52b84240c6919963a0df76d11635f7598acd
R.M. HildigerR V <moonsdad@gmail.com>
2013-09-01 21:53:36 -0700

    use strict - unsure if usage of my is appropriate, but it still works

--- CrazyLib.pl
+++ CrazyLib.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/perl -w
 
+use strict;
+
 #Select Input File
+my( $madlibf );
 if( $ARGV[0] ) { $madlibf = $ARGV[0]; }
 else {
     print "Enter madlib filename: ";
@@ -8,11 +11,12 @@ else {
     chomp $madlibf;
 }#end else
 open( INFILE, $madlibf ) || die "Opening $madlibf: $!\n";
-@madlib = <INFILE>;
+my @madlib = <INFILE>;
 close(INFILE);
 
 #Fill In Select Words
 foreach( @madlib ) {
+    my( $what, $ch0, $input );
     while( ($what) = m/<([^<]+)>/ ) {
         ($ch0) = split( //, $what );
         if( $ch0 =~ m/[aeiou]/i ) { print "Enter an $what: "; }
